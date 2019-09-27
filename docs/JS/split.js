ee8e9183878e52d3b92a919ac0ba69b52a3f87fa
Michael Catterall <43209825+MyCatt@users.noreply.github.com>
2019-09-28 00:30:01 +1200

    Update split.js

--- docs/JS/split.js
+++ docs/JS/split.js
@@ -1,9 +1,3 @@
-
-const API_KEY = 'AIzaSyDMbhCmhzrIsKXoGrxbqOTRzdoYiwxllag'
-const CLIENT_ID = '4137827372-07nd717u8ecpq74eetr5l2sidd3649nv.apps.googleusercontent.com'
-const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"]
-const SCOPES = "https://www.googleapis.com/auth/spreadsheets"
-
 const message = document.getElementById("message")
 const sheet_frame = document.getElementById("sheet_frame")
 const full_frame = document.getElementById("left-view")
